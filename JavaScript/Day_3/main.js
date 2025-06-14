61449a968c1b68e7d84746e230686522107370b8
developermickey <mukesh.quazma@gmail.com>
2025-06-14 13:05:40 +0530

    Conditional Statements

---
+++ JavaScript/Day_3/main.js
@@ -0,0 +1,128 @@
+// Logical Operator
+
+// Vote Dena Condition Age >= 18 VoterId = ture
+
+// AND &&
+// OR ||
+// NOT !
+
+// example for AND  &&
+
+// let age = 24;
+// let voterID = false;
+
+// if (age >= 18 && voterID) {
+//   console.log("App Vote De sakte ho!!!");
+// } else {
+//   console.log("App Vote Nahi De Sakte ho!!!");
+// }
+
+// if (age >= 18 || voterID) {
+//   console.log("App Vote De sakte ho!!!");
+// } else {
+//   console.log("App Vote Nahi De Sakte ho!!!");
+// }
+
+// let isStudent = true;
+
+// if (!isStudent) {
+//   console.log("Not a student");
+// } else {
+//   console.log("student");
+// }
+
+// e = 90 m = 95
+
+// let english = 90;
+// let maths = 94;
+
+// if (english >= 90 && maths >= 95) {
+//   console.log("Grade: A");
+// } else {
+//   console.log("Grade: B");
+// }
+
+// Bitwise Operator
+// AND & , Or | , XOR ^ , Not ~ , Left Shiift <<, Right Shift >>
+// Binary Base 0 1
+
+// console.log(5 & 3);
+
+//  0101  5
+//  0011  3
+//  0001   1
+
+// Ternary Operator ? :
+
+// condition ? expression_if_true : expression_if_false
+
+// if else
+
+// let age = 17;
+
+// let message = age >= 18 ? "Adult" : "Minor";
+
+// console.log(message);
+
+//  Conditions Statements
+
+// if(condition wo always true) {
+
+// }else{
+
+// }
+
+// if Student ka marks 90 to 100  A Grade,
+// if Student ka marks 80 < 90  B Grade,
+// if Student ka marks 60 < 80  C Grade,
+// if Student ka marks 35 < 60  D Grade,
+// if Student ka marks < 35  Fail,
+
+// let marks = 100;
+
+// if (marks >= 90 && marks <= 100) {
+//   console.log("Grade A");
+// } else if (marks >= 80 && marks < 90) {
+//   console.log("Grade B");
+// } else if (marks >= 60 && marks < 80) {
+//   console.log("Grade C");
+// } else if (marks >= 35 && marks < 60) {
+//   console.log("Grade D");
+// } else if (marks >= 1 && marks < 35) {
+//   console.log("Fail");
+// } else {
+//   console.log("Invaild Marks");
+// }
+
+// Write a program that checks if a person is eligible to vote. (Age must be 18 or above)
+
+// let age = 17;
+
+// if (age >= 18) {
+//   console.log("Yes you are eligible!!!");
+// } else {
+//   console.log("Yes you are not eligible!!!");
+// }
+
+// Write a program that takes a number an input and prints whether it is even or odd.
+
+// let number = prompt("Enter a any number");
+
+// if (number % 2 === 0) {
+//   console.log(`${number} is Even`);
+// } else {
+//   console.log(`${number} is Odd`);
+// }
+
+// Write a program that checks if a number is positive, negative, or zero.
+
+// let number = 10000000;
+
+// if (number < 0) {
+//   console.log(`${number} is Negative`);
+// } else if (number === 0) {
+//   console.log(`${number} is Zero`);
+// } else {
+//   console.log(`${number} is Positive`);
+// }
+//  Switch
