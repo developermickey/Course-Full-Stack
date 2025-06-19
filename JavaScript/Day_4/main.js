b9be1a5dc7202074944c2fdd3a1fefed786ef65b
developermickey <mukesh.quazma@gmail.com>
2025-06-19 21:50:39 +0530

    JavaScript Day 1, 2, 3, 4 Code added

---
+++ JavaScript/Day_4/main.js
@@ -0,0 +1,76 @@
+// Array and Methods
+
+// var num1 = 10;
+// var num2 = 20;
+// var num3 = 30;
+// var num4 = 40;
+// var num5 = 50;
+
+// How to create an Array
+
+// let arr = [10, 20, 30, 40, 50];
+
+// Access Elements
+
+// console.log(arr[2]);
+
+// What is length of the array
+
+// console.log(arr.length);
+// console.log(arr.length - 1);
+
+// How to add value in array
+
+// push();
+
+// arr.push(60);
+
+// console.log(arr);
+
+// pop();
+
+// arr.pop();
+
+// console.log(arr);
+
+// shift or unshift
+
+// unshift
+
+// arr.unshift(5);
+
+// console.log(arr);
+
+// arr.shift();
+
+// console.log(arr);
+
+// Sort the array
+
+// let num = [10, 20, 30, 40, 50];
+
+// console.log(num.sort());
+// console.log(num.sort((a, b) => b - a));
+
+// console.log(num);
+
+// console.log(num.splice(1, 2, 20, 30));
+// console.log(num);
+
+// slice();
+
+// console.log(num.slice(1, 3));
+// console.log(num);
+
+// concat()
+
+// let num1 = [1, 2, 3];
+// let num2 = [4, 5, 6];
+
+// let newArr = num2.concat(num1);
+
+// console.log(newArr);
+
+// let arr = [10, 20, 30, 40, 50];
+
+// console.log(arr.join("_"));
